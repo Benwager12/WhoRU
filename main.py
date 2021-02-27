8a768896d47d104af3867089efa7f3a17b070e88
benwager12 <benwager@live.co.uk>
2021-02-27 16:45:23 +0000

    Added UUID generator and UUID so it is unlikely people will break other's files.

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ from recognise_face import lookup
 from os import remove
 
 ###########################################
+# UUID Generator
+###########################################
+from random import randint
+
+alphabet = "abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+def uuid_char():
+	# Get a random character in the alphabet string.
+	return alphabet[randint(0, len(alphabet)-1)]
+
+def make_uuid(length=16):
+	# Join a bunch of characters of the length variable length.
+	# Those characters are called individually from the method "uuid_char"
+	return "".join([uuid_char() for x in range(length)])
+
+###########################################
 # Loading variables
 ###########################################
 import os
@@ -39,29 +55,34 @@ async def who(ctx):
 	if len(ctx.message.attachments) == 0:
 		return await ctx.reply("No attachments given.")
 
+	# Dictionary which contains the array of files to their respective UUID.
+	filesToUUID = {}
+
 	# Iterating through attachments and saving them, deleting later.
 	for attachment in ctx.message.attachments:
 		if not attachment.filename.lower().split(".")[-1] in image_types:
 			continue
-		await attachment.save("temporary-images/" + attachment.filename)
+
+		extension = "."+attachment.filename.split(".")[-1]
+		uuid = make_uuid()
+
+		filesToUUID[attachment.filename] = uuid+extension
+		await attachment.save(f"temporary-images/{uuid}{extension}")
 
 	# Iterating through attachments and saying who it looks like
-	for attachment in ctx.message.attachments:
-		name = lookup("temporary-images/" + attachment.filename)
+	for file in filesToUUID.keys():
+		name = lookup("temporary-images/" + filesToUUID[file])
 
 		if name == None:
-			return await ctx.reply("No faces in image.")
-			continue
+			await ctx.reply("No faces in image.")
 		elif name == ">1":
 			await ctx.reply("More than 1 face in image.")
-			continue
 		else:
-			file = discord.File("faces/"+name+".jpg")
-			await ctx.reply("The picture you sent (apparently) looks like " + name, file=file)
-
-	# Deleting attachments.
-	for attachment in ctx.message.attachments:
-		remove("temporary-images/" + attachment.filename)
+			face_file = discord.File("faces/"+name+".jpg")
+			await ctx.reply("The picture you sent (apparently) looks like " + name, file=face_file)
+		
+		# Delete the attachment after I'm finished with it.
+		remove("temporary-images/" + filesToUUID[file])
 
 ###########################################
 # Inserting token
